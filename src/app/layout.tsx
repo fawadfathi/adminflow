86db1ac00bb89e2ecb0c484862408002828136d3
Ahmad Fawad Fathi <153498498+fawadfathi@users.noreply.github.com>
2024-12-27 00:11:29 +0430

    admin dashboard

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import CustomSidebar from "@/components/custom-sidebar";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "AdminFlow",
+  description:
+    "AdminFlow Dashboard is a sleek, responsive admin panel for managing workflows, visualizing data, and optimizing operations with ease.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <CustomSidebar />
+      </body>
+    </html>
+  );
+}
---
+++ src/components/custom-sidebar.tsx
@@ -0,0 +1,86 @@
+import Link from "next/link";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+  {
+    title: "Task",
+    url: "#",
+  },
+  {
+    title: "Profile",
+    url: "#",
+  },
+  {
+    title: "Authentication",
+    url: "#",
+  },
+  {
+    title: "Settings",
+    url: "#",
+  },
+];
+
+const CustomSidebar = () => {
+  return (
+    <SidebarProvider>
+      <Sidebar collapsible="icon">
+        <SidebarContent>
+          <SidebarGroup>
+            <div className="flex mb-4">
+              <div className="bg-violet-600 p-1 rounded-md w-8">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
+                  />
+                </svg>
+              </div>
+              <SidebarGroupLabel className="text-xl">
+                AdminFlow
+              </SidebarGroupLabel>
+            </div>
+
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.url} className="text-xs">
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    </SidebarProvider>
+  );
+};
+
+export default CustomSidebar;
